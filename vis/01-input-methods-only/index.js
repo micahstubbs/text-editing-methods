c469ee0dff5c5d3518eedbbcf34e47fafd24d6bd
Micah Stubbs <micah.stubbs@gmail.com>
2018-09-01 21:39:16 -0700

    update 01 example

--- vis/01-input-methods-only/index.js
+++ vis/01-input-methods-only/index.js
@@ -14,18 +14,37 @@ d3.csv('text-editing-methods-speed.csv')
 function draw({ data }) {
   console.log('data', data)
 
+  const outerWidth = 960
+  const outerHeight = 500
+  const margin = { left: 50, top: 10, right: 50, bottom: 75 }
+  const themeDarkGray = '#444444'
+  const themeBookEmojiGray = '#dfdfe8'
+
   const selector = 'body'
   d3.select(selector)
     .append('div')
     .attr('id', 'vis')
     .append('svg')
+    .attr('width', outerWidth)
+    .attr('height', outerHeight)
+    .style('border', '1px solid #c3c3c3')
 
   const parent = document.getElementById('vis')
   const svg = d3.select(parent).select('svg')
 
-  const outerWidth = 960
-  const outerHeight = 500
-  const margin = { left: 165, top: 10, right: 50, bottom: 90 }
+  // background
+  svg
+    .append('rect')
+    .attr('x', 0)
+    .attr('y', 0)
+    .attr('width', outerWidth)
+    .attr('height', outerHeight)
+    .style('fill', themeBookEmojiGray)
+
+  const g = svg
+    .append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`)
+
   const innerWidth = outerWidth - margin.left - margin.right
   const innerHeight = outerHeight - margin.top - margin.bottom
 
@@ -35,17 +54,10 @@ function draw({ data }) {
   const yVariable = 'emoji'
 
   const xAxisLabelText = 'input speed, words per minute'
-  const xAxisLabelOffset = 75
+  const xAxisLabelOffset = 50
 
   console.log('width', outerWidth)
   console.log('height', outerHeight)
-
-  // use the the extracted size to set the size of an SVG element
-  svg.attr('width', outerWidth).attr('height', outerHeight)
-
-  const g = svg
-    .append('g')
-    .attr('transform', `translate(${margin.left},${margin.top})`)
 
   const xAxisG = g
     .append('g')
@@ -55,7 +67,7 @@ function draw({ data }) {
   const xAxisLabel = xAxisG
     .append('text')
     .style('text-anchor', 'middle')
-    .style('fill', 'black')
+    .style('fill', themeDarkGray)
     .style('font-size', 16)
     .attr('x', innerWidth / 2)
     .attr('y', xAxisLabelOffset)
